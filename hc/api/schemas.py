97c898e88a173be941118e1b26702b8c2c0512d2
LehruAngela <angelalehru@gmail.com>
2018-02-01 16:52:10 +0300

    [Fix]: resolving merge conflicts

--- hc/api/schemas.py
+++ hc/api/schemas.py
@@ -4,20 +4,9 @@ check = {
     "properties": {
         "name": {"type": "string"},
         "tags": {"type": "string"},
-<<<<<<< HEAD
-<<<<<<< HEAD
         "timeout": {"type": "number", "minimum": min_time, "maximum": max_time},
         "grace": {"type": "number", "minimum": min_time, "maximum": max_time},
         "nag": {"type": "number", "minimum": min_time, "maximum": max_time},
-=======
-        "timeout": {"type": "number", "minimum": 60, "maximum": 5184000},
-        "grace": {"type": "number", "minimum": 60, "maximum": 5184000},
->>>>>>> Ft increase timeouts and grace periods #153727847 (#20)
-=======
-        "timeout": {"type": "number", "minimum": min_time, "maximum": max_time},
-        "grace": {"type": "number", "minimum": min_time, "maximum": max_time},
-        "nag": {"type": "number", "minimum": min_time, "maximum": max_time},
->>>>>>> Nag interval Build Error Fix #153727845 (#19)
         "channels": {"type": "string"}
     }
 }
