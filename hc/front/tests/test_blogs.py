362f909ec200d1caa596afbe29a88d1733c2f9e2
LehruAngela <angelalehru@gmail.com>
2018-02-01 14:39:15 +0300

    [Fix:#153727855]Improved UI and added edit and delete feature to blog posts

--- hc/front/tests/test_blogs.py
+++ hc/front/tests/test_blogs.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # from hc.test import BaseTestCase
 # from hc.front.models import Blog_post, Category
 # from django.shortcuts import reverse
@@ -37,3 +38,48 @@
 #         }
 #         response_one = self.client.post(reverse('hc-create_blog'), form)
 #         self.assertEqual(response_one.status_code, 200)
+=======
+from hc.test import BaseTestCase
+from hc.front.models import Blog_post, Category
+from django.shortcuts import reverse
+from django.utils import timezone
+
+class BlogTest(BaseTestCase):
+    def setUp(self):
+        super(BlogTest, self).setUp()
+        self.client.login(username="alice@example.org", password="password")
+        self.category = Category(name='Tech')
+        self.category.save()
+        self.blog = Blog_post(title='Python',content='This is a blog on python', 
+                             category = self.category, published=timezone.now())
+        self.blog.save()
+   
+
+    def test_create_category(self):
+        """Test to check if categories are created"""
+        url = reverse('hc-create_blog')
+        data = {'category': ['Design'], 'new_category': ['']}
+        response = self.client.post(url, data)
+        query_category = Category.objects.get(name="Design")
+        self.assertEqual('Design', query_category.name)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'front/create_blog.html')
+
+
+    def test_create_blog(self):
+        """Test to check if blogs are created"""
+        url = reverse('hc-create_blog')
+        data = {'category_name': ['Tech'], 'title': ['Html'], 'content': ['This is a blog on html'], 'create_blog': ['']}
+        response = self.client.post(url, data)
+        query_blog = Blog_post.objects.get(title="Html")
+        self.assertEqual('Html', query_blog.title)
+        self.assertEqual(response.status_code, 302)
+
+
+    def test_home_page_returns_all_blogs(self):
+        """Test to check if home page returns all blogs"""
+        response = self.client.get(reverse('hc-blog', kwargs={'filter_by':0}))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'front/blog_posts.html')
+
+>>>>>>> [Fix:#153727855]Improved UI and added edit and delete feature to blog posts
--- hc/front/views.py
+++ hc/front/views.py
@@ -21,6 +21,8 @@ from hc.front.forms import (AddChannelForm, AddWebhookForm, NameTagsForm,
                             TimeoutForm)
 from .forms import CreateBlogPost, CreateCategory, CreateCommentForm
 from .models import Category, Blog_post, Comment
+from django.contrib import messages
+
 
 # from itertools recipes:
 def pairwise(iterable):
@@ -31,6 +33,10 @@ def pairwise(iterable):
 
 
 def blogs(request, filter_by):
+<<<<<<< HEAD
+=======
+    num = 3
+>>>>>>> [Fix:#153727855]Improved UI and added edit and delete feature to blog posts
     category = Category.objects.all()
     if int(filter_by) > 0:
         category_id = Category.objects.get(pk=filter_by).id
@@ -67,6 +73,7 @@ def create_blog(request):
             blog = Blog_post(title=title, content=blog, category=category,
                              published=published, user=user)
             blog.save()
+            messages.add_message(request, messages.INFO, 'Successfully created blog')
             return redirect(read_blog, pk=blog.id)
     categories = Category.objects.all()
     ctx = {
@@ -104,7 +111,9 @@ def read_blog(request, pk):
 
 
 def delete_blog(request, pk):
+    deleted_blog = Blog_post.objects.get(pk=pk)
     Blog_post.objects.get(pk=pk).delete()
+    messages.add_message(request, messages.INFO, 'Successfully deleted blog')
     return redirect(blogs, filter_by=0)
 
 
